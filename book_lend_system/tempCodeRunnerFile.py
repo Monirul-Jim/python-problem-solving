2d780c039bca7f89b7586f6c5f3c28419eb0f555
Monirul Jim <monirulislamjim6640@gmail.com>
2024-12-10 21:08:34 +0600

    make library system

---
+++ book_lend_system/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+ Book("100", "Introduction to Python Programming", "Guido van Rossum")
+    Book("101", "Introduction to C++", "Bjarne Stroustrup")
+    Book("102", "Javascript & Modern Web", "Brendan Eich")
