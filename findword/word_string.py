fbf7fd83a53b6fe2d7dab7c1a565271ac7fa5c68
Monirul Jim <monirulislamjim6640@gmail.com>
2025-01-04 20:39:21 +0600

    try to solve combinatons

--- findword/word_string.py
+++ findword/word_string.py
@@ -1,6 +1,6 @@
 
 from itertools import combinations
-user_input = input('Enter input : ')
+user_input = input('Enter your word : ')
 for r in range(1, len(user_input)+1):
     combs = combinations(user_input, r)
     for comb in combs:
